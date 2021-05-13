d18c1dd9616d3708d618651a7f4fcfb7c2226636
Neider <jhonacevedocardenas@iesantaelena.edu.co>
2021-05-13 17:47:57 -0500

    jsPostobon

--- postobon.js
+++ postobon.js
@@ -2,6 +2,7 @@ let etiquetaNombre=document.getElementById("nombreUsuario");
 let etiquetaHoras=document.getElementById("horasTrabajadas");
 let etiquetaExtras=document.getElementById("horasExtras");
 let botonCalcular=document.getElementById("botonCalcular");
+let resultado=document.getElementById("resultado");
 
 botonCalcular.addEventListener("click",Formulario);
 
@@ -10,9 +11,24 @@ function Formulario(){
     let cantidadHoras=Number(etiquetaHoras.value);
     let horasExtras=Number(etiquetaExtras.value);
 
-    if(nombre=="" && cantidadHoras==0 && horasExtras==0){
+    if(nombre=="" && cantidadHoras==0 && horasExtras==-1){
         console.log("Todos los campos están vacíos");
         etiquetaNombre.classList.add("is-invalid");
+        etiquetaHoras.classList.add("is-invalid");
+        etiquetaExtras.classList.add("is-invalid");
+    }else if (nombre=="" && cantidadHoras==0){
+        console.log("los campos nombre y horas trabajadas se encuentran vacíos");
+        etiquetaNombre.classList.add("is-invalid");
+        etiquetaHoras.classList.add("is-invalid");
+        etiquetaExtras.classList.remove("is-invalid");
+    }else if (nombre=="" && horasExtras==-1){
+        console.log("Los campos nombre, y horas Extras se encuentran vacíos");
+        etiquetaNombre.classList.add("is-invalid");
+        etiquetaHoras.classList.remove("is-invalid");
+        etiquetaExtras.classList.add("is-invalid");
+    }else if (cantidadHoras==0 && horasExtras==-1){
+        console.log("Los campos horas trabajadas y horas estrax se encuentra vacíos ")
+        etiquetaNombre.classList.remove("is-invalid");
         etiquetaHoras.classList.add("is-invalid");
         etiquetaExtras.classList.add("is-invalid");
     }else if (nombre==""){
@@ -25,7 +41,7 @@ function Formulario(){
         etiquetaNombre.classList.remove("is-invalid");
         etiquetaHoras.classList.add("is-invalid");
         etiquetaExtras.classList.remove("is-invalid");
-    }else if(horasExtras==0){
+    }else if(horasExtras==-1){
         console.log("El campo de horas extras se encuentra vacío");
         etiquetaNombre.classList.remove("is-invalid");
         etiquetaHoras.classList.remove("is-invalid");
@@ -35,7 +51,19 @@ function Formulario(){
         etiquetaNombre.classList.remove("is-invalid");
         etiquetaHoras.classList.remove("is-invalid");
         etiquetaExtras.classList.remove("is-invalid");
+        calcularSalario(cantidadHoras,horasExtras)
     }
 }
 
+function calcularSalario(cantidadHoras,horasExtras){
+    if (cantidadHoras>=1 && cantidadHoras<=40){
+        let vHora=cantidadHoras*20000;
+        resultado.textContent="El total se su sueldo es de:"+vHora;
+    }else {
+        let vExtras=horasExtras*25000;
+        let sueldo=vHora+vExtras;
+        resultado.textContent="El total de su sueldo es de:"+sueldo;
+    }
 
+    
+}
